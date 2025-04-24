263188cece9ecb65fccb1e1ac187b37c841c1458
mkutlu <mkutlu13@gmail.com>
2025-04-24 16:12:13 -0500

    initialize Kickstatz application with Maven setup and basic configuration

---
+++ src/main/java/com/kutlu/kickstatz/KickstatzApplication.java
@@ -0,0 +1,13 @@
+package com.kutlu.kickstatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickstatzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickstatzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kutlu/kickstatz/KickstatzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutlu.kickstatz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickstatzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
